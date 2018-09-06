d07c59e318db17e2f2893f061bde2f7568e8fd92
Setia Bhakti <joebhakti513@gmail.com>
2018-09-07 05:24:18 +0700

    Update promo.php

--- promo.php
+++ promo.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-$xml=simplexml_load_file("http://fetchrss.com/rss/5b85426d8a93f8f4548b4567218723325.xml") or die("Error: Cannot create object");
+$xml=simplexml_load_file("https://cdn.rawgit.com/ilham513/kafe-pdf/8ea499d6/promo.php") or die("Error: Cannot create object");
 print_r($xml);
 ?>
 
